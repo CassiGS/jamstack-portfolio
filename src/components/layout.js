7833b0fbc9b68e285f45d2a5a4f0d315c3c2b56a
Cassi <cassi.gallagher-shearer@submittable.com>
2023-02-16 18:28:58 -0800

    Add new layout styles and udpate homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import './variables.css'
-import './global.css'
-import Seo from './seo'
-import Navigation from './navigation'
-import Footer from './footer'
+import "./variables.css";
+import "./global.css";
+import Seo from "./seo";
+import Navigation from "./navigation";
+import Footer from "./footer";
+
+import * as styles from "./css/layout.module.css";
+
 class Template extends React.Component {
-  render() {
-    const { children } = this.props
+	render() {
+		const { children } = this.props;
 
-    return (
-      <>
-        <Seo />
-        <Navigation />
-        <main>{children}</main>
-        <Footer />
-      </>
-    )
-  }
+		return (
+			<>
+				<Seo />
+				<Navigation />
+				<main className={styles.container}>{children}</main>
+				<Footer />
+			</>
+		);
+	}
 }
 
-export default Template
+export default Template;
