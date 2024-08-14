3da3cecb31ccf4d6b15ce50042cf0572d36292d4
CassiGS <cassi.gallagher-shearer@submittable.com>
2024-08-13 17:56:26 -0700

    Adjust footer and home page styles'

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
 import Container from './container'
-import * as styles from './footer.module.css'
+import * as styles from './css/footer.module.css'
 
 const Footer = () => (
-  <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Built with <a href="https://contentful.com/">Contentful</a> and <a href="https://gatsbyjs.com">Gatsby</a>. Version 1.0
     </div>
-  </Container>
 )
 
 export default Footer
